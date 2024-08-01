75c34ee90b9dd81265e356ed4c7b47a8862fab71
Marlon Martins <marlon-martins@outlook.com>
2024-08-01 11:24:20 +0200

    React Testing Tutorial - 8 - Your First Test

--- src/components/Greet.test.tsx
+++ src/components/Greet.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import Greet from './Greet';
 
-test('renders learn react link', () => {
+test('Greet renders correctly', () => {
   render(<Greet />);
   const textElement = screen.getByText(/greet/i);
   expect(textElement).toBeInTheDocument();
